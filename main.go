9d8202d71081dc9c2b2d660141578a14924a088c
R-JoniJoni <gencatochan@gmail.com>
2022-03-28 13:56:18 +0800

    获取Nodes的功能完善成功。

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"douban-movies-top250-web-crawler/page"
+	"encoding/csv"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"math/rand"
@@ -66,39 +68,109 @@ func getRelations() {
 }
 
 func getNodes() {
+	fmt.Println("Now getting Nodes.")
 	id := 0
 	directors := make(map[string]int, 0)
 	actors := make(map[string]int, 0)
 	films := make(map[string]int, 0)
-	types := make(map[string]int, 0)
+	movieTypes := make(map[string]int, 0)
 
-	for i := 0; i < 250; i++ {
+	for i := 0; i < 250; i++ { // 循环在每个txt文件中找
 		file, err := os.Open("data/contents/movie" + strconv.Itoa(i) + ".txt")
 		if err != nil {
 			continue
 		}
-
-		page, err := ioutil.ReadAll(file)
+		moviePage, err := ioutil.ReadAll(file)
 		if err != nil {
 			log.Fatalf("cannot read all from file: %v\n", err)
 		}
 
 		// 找电影名
-		regExp := regexp.MustCompile(``)
-		film := regExp.FindString(string(page))
-		film = strings.TrimSuffix(film, `"name": "`)
-		film = strings.TrimSuffix(film, `",`)
-		films[film] = id
-		id++
+		regExp := regexp.MustCompile(`<title> {8}.*? \(豆瓣\)</title>`)
+		film := regExp.Find(moviePage)
+		filmStr := string(film)
+		filmStr = strings.TrimPrefix(filmStr, `<title>        `)
+		filmStr = strings.TrimSuffix(filmStr, ` (豆瓣)</title>`)
+		if _, ok := films[filmStr]; !ok {
+			films[filmStr] = id
+			id++
+		}
 
 		// 找导演名
+		regExp = regexp.MustCompile(`"director":.*?]`)
+		directorNoise := regExp.Find(moviePage)
+		regExp = regexp.MustCompile(`"name": ".*?"`)
+		director := regExp.FindAll(directorNoise, -1)
+		for _, v := range director {
+			directorStr := string(v)
+			directorStr = strings.TrimPrefix(directorStr, `"name": "`)
+			directorStr = strings.TrimSuffix(directorStr, `"`)
+			if _, ok := directors[directorStr]; !ok {
+				directors[directorStr] = id
+				id++
+			}
+		}
 
 		// 找演员名
+		regExp = regexp.MustCompile(`"actor":.*?]`)
+		actorNoise := regExp.Find(moviePage)
+		regExp = regexp.MustCompile(`"name": ".*?"`)
+		actor := regExp.FindAll(actorNoise, -1)
+		for _, v := range actor {
+			actorStr := string(v)
+			actorStr = strings.TrimPrefix(actorStr, `"name": "`)
+			actorStr = strings.TrimSuffix(actorStr, `"`)
+			if _, ok := actors[actorStr]; !ok {
+				actors[actorStr] = id
+				id++
+			}
+		}
+
 		// 找类型名
+		regExp = regexp.MustCompile(`<span property="v:genre">.*?</span>`)
+		movieType := regExp.FindAll(moviePage, -1)
+		for _, v := range movieType {
+			typeStr := string(v)
+			typeStr = strings.TrimPrefix(typeStr, `<span property="v:genre">`)
+			typeStr = strings.TrimSuffix(typeStr, `</span>`)
+			if _, ok := movieTypes[typeStr]; !ok {
+				movieTypes[typeStr] = id
+				id++
+			}
+		}
 
 		file.Close()
-
 	}
+
+	// 存csv文件
+	saveCsv(films, "film")
+	saveCsv(actors, "actor")
+	saveCsv(directors, "director")
+	saveCsv(movieTypes, "type")
+
+	fmt.Println("All Nodes have been written into csv files.")
+}
+
+func saveCsv(myMap map[string]int, fileName string) {
+	csvFile, err := os.Create("data/details/" + fileName + ".csv")
+	if err != nil {
+		log.Fatalf("cannot open csv file %s: %v\n", fileName, err)
+	}
+	defer csvFile.Close()
+
+	// 获取csv的Writer
+	writer := csv.NewWriter(csvFile)
+	// 并写入csv文件
+	for name, id := range myMap {
+		oneLine := []string{name, strconv.Itoa(id)}
+		err := writer.Write(oneLine)
+		if err != nil {
+			log.Fatalf("cannot write csv file: %v\n", err)
+		}
+	}
+
+	// 确保所有内存数据刷到csv文件
+	writer.Flush()
 }
 
 func crawlMoviesAndSaveTxt(urls [][]string, i int, userAgents []string) {
@@ -109,9 +181,30 @@ func crawlMoviesAndSaveTxt(urls [][]string, i int, userAgents []string) {
 			UserAgent: userAgents[rand.Intn(len(userAgents))],
 		}
 		movieBody := movieRobot.Crawl()
+		movieBody = CutNewLine(movieBody)                                                           // 为了之后正则表达式匹配方便，去掉所有的换行符
 		err := ioutil.WriteFile("data/contents/movie"+strconv.Itoa(i*25+j)+".txt", movieBody, 0644) // 把爬到的页面HTML存入txt文件中
 		if err != nil {
 			log.Fatalf("cannot write in file: %v", err)
 		}
 	}
+}
+
+func CutNewLine(body []byte) []byte {
+	count := 0
+	for _, v := range body {
+		if v == '\n' {
+			count++
+		}
+	}
+
+	nb := make([]byte, len(body)-count)
+	for i, j := 0, 0; i < len(nb); {
+		if body[j] != '\n' {
+			nb[i] = body[j]
+			i++
+		}
+		j++
+	}
+
+	return nb
 }
